b22ad7cf9f574b46b668e850a887f22ebb6c6496
jjprz <jj.p3rez@gmail.com>
2021-06-01 22:47:41 +0200

    Update EdistribucionAPI.py

--- backend/EdistribucionAPI.py
+++ backend/EdistribucionAPI.py
@@ -2,23 +2,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed May 20 11:42:56 2020
-
 @author: trocotronic
 """
 
-__VERSION__ = 0.4
-
-import requests, pickle, json, os, math
+import requests, pickle, json
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, unquote
 import logging
 from datetime import datetime, timedelta
-from dateutil.tz import tzutc
-
-UTC = tzutc()
-
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-logging.getLogger().setLevel(logging.DEBUG)
 
 class EdisError(Exception):
     def __init__(self, message):
@@ -32,18 +24,6 @@ class UrlError(EdisError):
         super().__init__(message)
     pass
 
-def serialize_date(dt):
-    """
-    Serialize a date/time value into an ISO8601 text representation
-    adjusted (if needed) to UTC timezone.
-
-    For instance:
-    >>> serialize_date(datetime(2012, 4, 10, 22, 38, 20, 604391))
-    '2012-04-10T22:38:20.604391Z'
-    """
-    if dt.tzinfo:
-        dt = dt.astimezone(UTC).replace(tzinfo=None)
-    return dt.isoformat() 
 
 class Edistribucion():
     __session = None
@@ -56,13 +36,12 @@ class Edistribucion():
     __identities = {}
     __appInfo = None
     __context = None
-    __access_date = datetime.now()
     
     def __init__(self, user, password, debug_level=logging.INFO):
         self.__session = requests.Session()
         self.__credentials['user'] = user
         self.__credentials['password'] = password
-        
+        ''' Disable sesion and toke files
         try:
             with open(Edistribucion.SESSION_FILE, 'rb') as f:
                 self.__session.cookies.update(pickle.load(f))
@@ -74,10 +53,9 @@ class Edistribucion():
                 self.__token = d['token']
                 self.__identities = d['identities']
                 self.__context = d['context']
-                self.__access_date = datetime.fromisoformat(d['date'])
         except FileNotFoundError:
             logging.warning('Access file not found')
-        
+        '''
         logging.getLogger().setLevel(debug_level)
         
     def __get_url(self, url,get=None,post=None,json=None,cookies=None,headers=None):
@@ -113,7 +91,7 @@ class Edistribucion():
             raise UrlError(r.status_code, msg, r)
         return r
     
-    def __command(self, command, post=None, dashboard=None, accept='*/*', content_type=None, recursive=False):
+    def __command(self, command, post=None, dashboard=None, accept='*/*', content_type=None, recurrent=False):
         if (not dashboard):
             dashboard = self.__dashboard
         if (self.__command_index):
@@ -130,37 +108,31 @@ class Edistribucion():
             logging.debug('Accept: %s', accept)
         if (content_type):
             logging.debug('Content-tpye: %s', content_type)
-            '''
         try:
             if (not self.__check_tokens()):
                 self.__force_login()
         except UrlError as e:
             raise EdisError('Aborting command {}: login failed ({})'.format(command,e.message))
-            '''
         headers = {}
         if (content_type):
             headers['Content-Type'] = content_type
         if (accept):
             headers['Accept'] = accept
         r = self.__get_url(dashboard+command, post=post, headers=headers)
-        if ('window.location.href' in r.text or 'clientOutOfSync' in r.text):
-            if (not recursive):
+        if ('window.location.href' in r.text):
+            if (not recurrent):
                 logging.info('Redirection received. Fetching credentials again.')
-                
-                self.__session = requests.Session()
                 self.__force_login()
-                self.__command(command=command, post=post, dashboard=dashboard, accept=accept, content_type=content_type, recursive=True)
+                self.__command(command=command, post=post, dashboard=dashboard, accept=accept, content_type=content_type, recurrent=True)
             else:
                 logging.warning('Redirection received twice. Aborting command.')
         if ('json' in r.headers['Content-Type']):
             jr = r.json()
             if (jr['actions'][0]['state'] != 'SUCCESS'):
-                if (not recursive):
+                if (not recurrent):
                     logging.info('Error received. Fetching credentials again.')
-                    
-                    self.__session = requests.Session()
                     self.__force_login()
-                    self.__command(command=command, post=post, dashboard=dashboard, accept=accept, content_type=content_type, recursive=True)
+                    self.__command(command=command, post=post, dashboard=dashboard, accept=accept, content_type=content_type, recurrent=True)
                 else:
                     logging.warning('Error received twice. Aborting command.')
                     raise EdisError('Error processing command: {} ({})'.format(jr['actions'][0]['error'][0]['message'],jr['actions'][0]['error'][0]['exceptionType']))
@@ -169,27 +141,25 @@ class Edistribucion():
     
     def __check_tokens(self):
         logging.debug('Checking tokens')
-        return self.__token != 'undefined' and self.__access_date+timedelta(minutes=10) > datetime.now()
+        return self.__token != 'undefined'
         
     def __save_access(self):
         t = {}
         t['token'] = self.__token
         t['identities'] = self.__identities
         t['context'] = self.__context
-        t['date'] = datetime.now()
-        with open(Edistribucion.ACCESS_FILE, 'w') as f:
-            json.dump(t, f, default=serialize_date)
-        logging.info('Saving access to file')
+        #with open(Edistribucion.ACCESS_FILE, 'w') as f:
+        #    json.dump(t, f)
+        #logging.info('Saving access to file')
         
     def login(self):
-        logging.info('Loging')
+        logging.info('Logging')
         if (not self.__check_tokens()):
-            self.__session = requests.Session()
             return self.__force_login()
         return True
     
-    def __force_login(self, recursive=False):
-        logging.warning('Forcing login')
+    def __force_login(self):
+        #logging.warning('Forcing login')
         r = self.__get_url('https://zonaprivada.edistribucion.com/areaprivada/s/login?ec=302&startURL=%2Fareaprivada%2Fs%2F')
         ix = r.text.find('auraConfig')
         if (ix == -1):
@@ -219,9 +189,6 @@ class Edistribucion():
         r = self.__get_url(self.__dashboard+'other.LightningLoginForm.login=1',post=data)
         print(r.text)
         if ('/*ERROR*/' in r.text):
-            if ('invalidSession' in r.text and not recursive):
-                self.__session = requests.Session()
-                self.__force_login(recursive=True)
             raise EdisError('Unexpected error in loginForm. Cannot continue')
         jr = r.json()
         if ('events' not in jr):
@@ -247,11 +214,11 @@ class Edistribucion():
         self.__identities['name'] = r['Name']
         logging.info('Received name: %s (%s)',r['Name'],r['visibility']['Visible_Account__r']['Identity_number__c'])
         logging.debug('Account_id: %s', self.__identities['account_id'])
-        with open(Edistribucion.SESSION_FILE, 'wb') as f:
-            pickle.dump(self.__session.cookies, f)
-        logging.debug('Saving session')
+        #with open(Edistribucion.SESSION_FILE, 'wb') as f:
+        #    pickle.dump(self.__session.cookies, f)
+        #logging.debug('Saving session')
         self.__save_access()
-            
+
     def get_login_info(self):
         data = {
             'message': '{"actions":[{"id":"215;a","descriptor":"apex://WP_Monitor_CTRL/ACTION$getLoginInfo","callingDescriptor":"markup://c:WP_Monitor","params":{"serviceNumber":"S011"}}]}',
@@ -261,7 +228,7 @@ class Edistribucion():
         
     def get_cups(self):
         data = {
-            'message': '{"actions":[{"id":"270;a","descriptor":"apex://WP_ContadorICP_CTRL/ACTION$getCUPSReconectarICP","callingDescriptor":"markup://c:WP_Reconnect_ICP","params":{"visSelected":"'+self.__identities['account_id']+'"}}]}',
+            'message': '{"actions":[{"id":"419;a","descriptor":"apex://WP_ContadorICP_CTRL/ACTION$getCUPSReconectarICP","callingDescriptor":"markup://c:WP_Reconnect_ICP","params":{"visSelected":"'+self.__identities['account_id']+'"}}]}',
             }
         r = self.__command('other.WP_ContadorICP_CTRL.getCUPSReconectarICP=1', post=data)
         return r
@@ -314,7 +281,7 @@ class Edistribucion():
             }
         r = self.__command('other.WP_SolicitudATRDetail_CTRL.getSolicitudATRDetail=1', post=data)
         return r
-    
+
     def reconnect_ICP(self, cups):
         data = {
             'message': '{"actions":[{"id":"261;a","descriptor":"apex://WP_ContadorICP_F2_CTRL/ACTION$reconectarICP","callingDescriptor":"markup://c:WP_Reconnect_Detail_F2","params":{"cupsId":"'+cups+'"}}]}',
@@ -340,7 +307,6 @@ class Edistribucion():
                 c['Id'] = cont['Id']
                 c['Active'] = False if 'Version_end_date__c' in cont else True
                 c['Power'] = cont['Requested_power_1__c']
-                c['Rate'] = cont['rate']
                 conts.append(c)
         return conts
 
@@ -350,13 +316,24 @@ class Edistribucion():
             }
         r = self.__command('other.WP_Measure_v3_CTRL.getInfo=1', post=data)
         return r['data']['lstCycles']
-        
-      
-    def get_meas(self, cont, cycle):
-        data = {
-            'message': '{"actions":[{"id":"1295;a","descriptor":"apex://WP_Measure_v3_CTRL/ACTION$getChartPoints","callingDescriptor":"markup://c:WP_Measure_Detail_v4","params":{"cupsId":"'+cont['Id']+'","dateRange":"'+cycle['label']+'","cfactura":"'+cycle['value']+'"},"longRunning":true}]}',
-            }
-        r = self.__command('other.WP_Measure_v3_CTRL.getChartPoints=1', post=data)
-        return r['data']['lstData']
+    
+    def get_maximeter_histogram(self, cups):
 
+        today = datetime.today()
+        dStart = str(today.month) + '/' + str(today.year - 2)
+        dEnd = str(today.month) + '/' + str(today.year)
+        data = {
+            'message': '{"actions":[{"id":"490;a","descriptor":"apex://WP_MaximeterHistogram_CTRL/ACTION$getHistogramPoints","callingDescriptor":"markup://c:WP_MaximeterHistogramDetail","params":{"mapParams":{"startDate":"'+ dStart +'","endDate":"' + dEnd + '","id":"'+cups+'","sIdentificador":"'+self.__identities['account_id']+'"}}}]}',
+            }
+        r = self.__command('other.WP_MaximeterHistogram_CTRL.getHistogramPoints=1', post=data)
+        return r
+
+    def get_chart_points_by_range(self, cont, sStart, sEnd):
+
+        data = {
+            'message': '{"actions":[{"id":"949;a","descriptor":"apex://WP_Measure_v3_CTRL/ACTION$getChartPointsByRange","callingDescriptor":"markup://c:WP_Measure_Detail_Filter_Advanced_v3","params":{"contId":"'+cont['Id']+'","type":"4","startDate":"' + sStart + '","endDate":"' + sEnd + '"},"version":null,"longRunning":true}]}',
+            }
+        r = self.__command('other.WP_Measure_v3_CTRL.getChartPointsByRange=1', post=data)
+        return r
+        
         
